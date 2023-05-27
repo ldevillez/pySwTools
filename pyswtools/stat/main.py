ebaed3d40f5b852edf67c714dee30bd6dfdbc16e
ldevillez <louis.devillez@gmail.com>
2023-05-27 13:42:24 +0200

    add comments

--- pyswtools/stat/main.py
+++ pyswtools/stat/main.py
@@ -3,11 +3,27 @@ Give stat from an assembly
 """
 
 import os
+from enum import Enum
 import click
 
 # pylint: disable=relative-beyond-top-level
 from ..utils import check_system, check_system_verbose
 from ..config import get_config
+
+
+class TypeOutput(Enum):
+    """Class represeting an output type"""
+
+    TREE = 1
+    LIST = 2
+
+
+class TypeSort(Enum):
+    """Class represeting an output type"""
+
+    MASS = 1
+    NAME = 2
+
 
 if check_system():
     # pylint: disable=import-error
@@ -18,16 +34,14 @@ if check_system():
     VT_Views = win32com.client.VARIANT(pythoncom.VT_VARIANT, Views)
     VT_BYREF = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, -1)
 
-# TODO define type_sort and type_output as dataclass
 
-
-def sort_key_struct(struct, type_sort):
+def sort_key_struct(struct: dict, type_sort: TypeSort = TypeSort.MASS) -> list:
     """
     Sort a dict struct following a given sort
     """
-    if type_sort == "name":
+    if type_sort == TypeSort.NAME:
         return sorted(struct.keys(), key=str.lower)
-    if type_sort == "mass":
+    if type_sort == TypeSort.MASS:
         return sorted(
             struct.keys(),
             key=lambda i: struct[i]["mass"] * struct[i]["number"],
@@ -37,7 +51,9 @@ def sort_key_struct(struct, type_sort):
     return struct.keys()
 
 
-def display_tree(tree_struct_mass, type_sort="name", indent=0):
+def display_tree(
+    tree_struct_mass: dict, type_sort: TypeSort = TypeSort.NAME, indent: int = 0
+) -> None:
     """
     Display a tree struct recursively
     """
@@ -49,7 +65,7 @@ def display_tree(tree_struct_mass, type_sort="name", indent=0):
             display_tree(tree_struct_mass[elem]["children"], type_sort, indent + 1)
 
 
-def display_list(list_struct, type_sort="name"):
+def display_list(list_struct: dict, type_sort: TypeSort = TypeSort.NAME) -> None:
     """
     Display a list struct
     """
@@ -59,21 +75,26 @@ def display_list(list_struct, type_sort="name"):
         )
 
 
-def get_clean_name(sw_comp):
+def get_clean_name(sw_comp) -> str:
     """
-    Get the cleaned name from a component
+    Get the cleaned name from a component. remove the number from the component
     """
     return sw_comp.Name2.rpartition("-")[0].rpartition("/")[-1]
 
 
-def complete_info_on_list(sw_comp_children, dict_of_comp):
+def complete_info_on_list(sw_comp_children, dict_of_comp: dict) -> dict:
     """
     Get all the info from a list of component
     """
     children = {}
     for sw_child in sw_comp_children:
+        # Get the name
         sw_child_name = get_clean_name(sw_child)
+
+        # Get the info fo the assembly
         child = complete_info_assembly(sw_child, dict_of_comp)
+
+        # If the child is already here, only increase the number
         if sw_child_name not in children:
             children[sw_child_name] = child
         else:
@@ -82,23 +103,30 @@ def complete_info_on_list(sw_comp_children, dict_of_comp):
     return children
 
 
-def complete_info_assembly(sw_comp, dict_of_comp):
+def complete_info_assembly(sw_comp, dict_of_comp: dict) -> dict:
     """
     Get all the information from an assembly
     """
+
+    # Get name of component
     sw_comp_name = get_clean_name(sw_comp)
 
+    # If already there only increase the number
     if sw_comp_name in dict_of_comp:
         dict_of_comp[sw_comp_name]["number"] += 1
     else:
+        # Otherwise get the mass
         sw_comp_doc_ext = sw_comp.GetModelDoc2.Extension
         sw_mass = sw_comp_doc_ext.CreateMassProperty2.Mass
 
+        # Create an new entity in the general dict
         dict_of_comp[sw_comp_name] = {"number": 1, "mass": sw_mass}
 
+    # Get info about children
     sw_comp_children = sw_comp.GetChildren
     children = complete_info_on_list(sw_comp_children, dict_of_comp)
 
+    # Create the entry in the tree dict
     return {
         "mass": dict_of_comp[sw_comp_name]["mass"],
         "number": 1,
@@ -115,11 +143,11 @@ def complete_info_assembly(sw_comp, dict_of_comp):
         dir_okay=False,
     ),
 )
-@click.option("--tree", "type_output", flag_value="tree", default=True)
-@click.option("--list", "type_output", flag_value="list")
-@click.option("--mass", "type_sort", flag_value="mass", default=True)
-@click.option("--name", "type_sort", flag_value="name")
-def stat(input_path, type_output, type_sort) -> None:
+@click.option("--tree", "type_output", flag_value=TypeOutput.TREE, default=True)
+@click.option("--list", "type_output", flag_value=TypeOutput.LIST)
+@click.option("--mass", "type_sort", flag_value=TypeSort.MASS, default=True)
+@click.option("--name", "type_sort", flag_value=TypeSort.NAME)
+def stat(input_path: str, type_output: TypeOutput, type_sort: TypeSort) -> None:
     """
     Display stat about an assembly
     """
@@ -154,9 +182,9 @@ def stat(input_path, type_output, type_sort) -> None:
     # Get all the infos
     tree_of_infos = complete_info_on_list(sw_comps, dict_of_comp)
 
-    if type_output == "tree":
+    if type_output == TypeOutput.TREE:
         display_tree(tree_of_infos, type_sort)
-    elif type_output == "list":
+    elif type_output == TypeOutput.LIST:
         display_list(dict_of_comp, type_sort)
     else:
         pass
